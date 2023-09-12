449bb3e5d857a7f3fb328de9f0e3559e75924634
Daniel Viveros <dviveros90@gmail.com>
2023-09-12 12:03:50 -0400

    Tests for Displaying Alerts

---
+++ xamarin.quest.course.tests/Main/DialogMessageMock.cs
@@ -0,0 +1,15 @@
+﻿using xamarin.quest.course.Dialog;
+
+namespace xamarin.quest.course.tests.Main
+{
+    public class DialogMessageMock : IDialogMessage
+    {
+        public int DisplayAlertCallCount { get; set; }
+
+        public async Task DisplayAlert(string title, string message, string cancel)
+        {
+            this.DisplayAlertCallCount++;
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ xamarin.quest.course.tests/Main/MainViewModelTests.cs
@@ -0,0 +1,18 @@
+﻿using xamarin.quest.course.Main;
+
+namespace xamarin.quest.course.tests.Main
+{
+    public class MainViewModelTests
+    {
+        [Fact]
+        public void DisplayAlertCommand_WhenCalled_DisplaysAlert()
+        {
+            var dialogMessage = new DialogMessageMock();
+            var viewModel = new MainViewModel(dialogMessage);
+
+            viewModel.DisplayAlertCommand.Execute(null);
+
+            Assert.Equal(1, dialogMessage.DisplayAlertCallCount);
+        }
+    }
+}
