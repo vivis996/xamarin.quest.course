ba14001c12e3202288aec431d9a9a72a9f27d633
Daniel Viveros <dviveros90@gmail.com>
2023-09-14 10:36:23 -0400

    Basics of Networking

---
+++ xamarin.quest.course.part2/xamarin.quest.course.part2/NetworkService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace xamarin.quest.course.part2
+{
+    public class NetworkService
+    {
+        private HttpClient _httpClient;
+
+        public NetworkService()
+        {
+            this._httpClient = new HttpClient();
+        }
+
+        public async Task<TResult> GetAsync<TResult>(string uri)
+        {
+            var response = await this._httpClient.GetAsync(uri);
+            var serialized = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<TResult>(serialized);
+            return result;
+        }
+    }
+}
